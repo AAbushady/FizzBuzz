55c89b3c56ec56ad50e76a017d868c33d0a2f52e
Alex Abushady <aabushady214@gmail.com>
2019-09-19 09:31:51 -0400

    Update fizzBuzz.js
    Seperate Fizz and Buzz implemented.

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -6,3 +6,15 @@ number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
 When that is working, modify to print "FizzBuzz" for numbers that are divisible by
 both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only those).
 */
+
+const loopAmount = 100;
+
+for(cnt = 1; cnt <= loopAmount; cnt ++) {
+    if (cnt % 3 == 0) {
+        console.log("Fizz");
+    } else if (cnt % 5 == 0) {
+        console.log("Buzz");
+    } else {
+        console.log(cnt);
+    }
+}
