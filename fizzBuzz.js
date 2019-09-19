ed58bde3332716c6885732d78d58a1621a0fdf79
Alex Abushady <aabushady214@gmail.com>
2019-09-19 09:24:50 -0400

    Create fizzBuzz.js
    Added problem statement.

---
+++ fizzBuzz.js
@@ -0,0 +1,8 @@
+/*
+Write a program that uses console.log to pring all the numbers from 1 to 100, 
+with two exceptions. For numbers divisible by 3, print "Fizz" instead of the
+number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
+
+When that is working, modify to print "FizzBuzz" for numbers that are divisible by
+both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only those).
+*/
