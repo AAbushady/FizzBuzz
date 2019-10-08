13a10c5e1ffcf6444b1e4d396d292f3a3fc0620d
Alex Abushady <aabushady214@gmail.com>
2019-10-08 14:02:03 -0400

    Update fizzBuzz.js
    Wrapped script in a function, started using strict, and cleaned up code.

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,22 +1,20 @@
-/*
-Write a program that uses console.log to pring all the numbers from 1 to 100, 
-with two exceptions. For numbers divisible by 3, print "Fizz" instead of the
-number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
+/*global console*/
+/*jshint esversion:6*/
 
-When that is working, modify to print "FizzBuzz" for numbers that are divisible by
-both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only those).
-*/
+(function () {
+    'use strict';
 
-const loopAmount = 100;
+    const loopAmount = 100;
 
-for (cnt = 1; cnt <= loopAmount; cnt++) {
-    if (cnt % 3 == 0 && cnt % 5 == 0) {
-        console.log("FizzBuzz");
-    } else if (cnt % 3 == 0) {
-        console.log("Fizz");
-    } else if (cnt % 5 == 0) {
-        console.log("Buzz");
-    } else {
-        console.log(cnt);
+    for (let cnt = 1; cnt <= loopAmount; cnt++) {
+        if (cnt % 3 === 0 && cnt % 5 === 0) {
+            console.log("FizzBuzz");
+        } else if (cnt % 3 === 0) {
+            console.log("Fizz");
+        } else if (cnt % 5 === 0) {
+            console.log("Buzz");
+        } else {
+            console.log(cnt);
+        }
     }
-}
+})();
